dacd10e9c0b0effae568299fa49b270bf0540620
Kariuki <mkmartinoes@gmail.com>
2025-06-30 21:30:01 +0300

    st: project section

---
+++ components/Projects.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Carousel, CarouselSlide } from '@mantine/carousel'
+import ProjectCard from './ProjectCard'
+
+const projects = [
+  {
+    name: 'Nexcom',
+    url: 'https://nexcom.site/',
+    description: 'High perfomant Super App',
+    github: 'https://github.com/kariuki-kariuki/nexcom-web',
+    imageUrl: '/projects/nexcom.png',
+    techStack: ["React", "Redis", "HTML", "NestJs", "Mantine"]
+  },
+  {
+    name: 'Scode Social',
+    url: 'https://social.scode.co.ke/',
+    description: 'NGO Website Platform',
+    github: 'https://github.com/kariuki-kariuki/',
+    imageUrl: '/projects/social.png',
+    techStack: ["Nextjs", "Tailwind", "NestJs", "Mantine"]
+
+  },
+  {
+    name: 'Scode Enterprise',
+    url: 'https://enterprise.scode.co.ke/',
+    description: 'NGO Ecomerce Platform',
+    github: 'https://github.com/kariuki-kariuki/',
+    imageUrl: '/projects/enterprise.png'
+  },
+  {
+    name: 'Nexcom Dashboard',
+    url: 'https://nexcom.site/',
+    description: 'High perfomant Super Dashboard',
+    github: 'https://github.com/kariuki-kariuki/nexcom-web',
+    imageUrl: '/projects/dashboard.png'
+  },
+  {
+    name: 'Nexcom2',
+    url: 'https://nexcom.site/',
+    description: 'High perfomant Super App',
+    github: 'https://github.com/kariuki-kariuki/nexcom-web',
+    imageUrl: '/projects/nexcom.png'
+  },
+
+]
+
+const Projects = () => {
+  const slides = projects.map((project) => (
+    <CarouselSlide key={project.name} my="xl">
+      <ProjectCard project={project} />
+    </CarouselSlide>
+  ))
+  return (
+    <div className='min-h-screen flex flex-col  p-5'>
+      <div className='py-24 container mx-auto'>
+        <div className='flex gap-2 justify-start items-center'>
+          <i className='w-20 h-1 bg-orange-600'></i>
+          <h1 className='text-2xl lg:text-4xl font-bold text-black'>Projects</h1>
+        </div>
+        <p className='text-2xl text-gray-600'>Some Projects I have worked on</p>
+      </div>
+
+
+      <div>
+        <Carousel slideSize={{ base: '80%', sm: '33.333%', lg: '20%' }} emblaOptions={{ loop: true, align: 'start' }} slideGap="xl" withControls>
+          {slides}
+        </Carousel>
+      </div>
+
+    </div>
+  )
+}
+
+export default Projects
