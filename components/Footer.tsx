b4a34cb33db18980c003b161d5a61859b07b79a6
Kariuki <mkmartinoes@gmail.com>
2025-06-30 21:30:45 +0300

    st: footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='h-[100px] flex flex-col justify-center bg-[#FFFDF6] sticky border-t border-gray-300'>
+      <p className='text-center'>&copy; 2025 Martin Kariuki</p>
+    </div>
+  )
+}
+
+export default Footer
